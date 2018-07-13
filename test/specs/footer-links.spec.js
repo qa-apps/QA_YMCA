d38c36bde39600a65034a4b96b80171c493cadeb
Pavels Smoljaninovs <pasok.a@yahoo.com>
2018-07-13 15:18:00 -0500

    test(footer): validate footer presence and key links

---
+++ test/specs/footer-links.spec.js
@@ -0,0 +1,30 @@
+const HomePage = require('../pageobjects/HomePage');
+
+describe('Footer links', () => {
+  it('footer exists and contains expected columns', async () => {
+    await HomePage.open();
+    const footer = await $('footer');
+    await expect(footer).toBeExisting();
+    const cols = await footer.$$('a, button');
+    expect(cols.length).toBeGreaterThan(5);
+  });
+
+  it('includes links like Privacy Policy or Terms of Use', async () => {
+    await HomePage.open();
+    const privacy = await $('//a[contains(. , \"Privacy\")]');
+    const terms = await $('//a[contains(. , \"Terms\")]');
+    expect((await privacy.isExisting()) || (await terms.isExisting())).toBe(true);
+  });
+
+  it('clicks Financial Information if present', async () => {
+    await HomePage.open();
+    const fin = await $('//a[contains(. , \"Financial Information\")]');
+    if (await fin.isExisting()) {
+      await fin.click();
+      await browser.pause(300);
+      await expect(browser).toHaveTitleContaining('Financial');
+    }
+  });
+});
+
+
