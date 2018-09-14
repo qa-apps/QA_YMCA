710530f129496629398feccf30da8655c27d91dd
Pavels Smoljaninovs <pasok.a@yahoo.com>
2018-09-14 14:14:00 -0500

    test(footer): basic columns and link visibility

---
+++ test/specs/footer-columns.spec.js
@@ -0,0 +1,31 @@
+const HomePage = require('../pageobjects/HomePage');
+
+describe('Footer column structure', () => {
+  it('footer has multiple columns of links', async () => {
+    await HomePage.open();
+    const footer = await $('footer');
+    await expect(footer).toBeExisting();
+    const columns = await footer.$$('nav, ul, div:has(a)');
+    expect(columns.length).toBeGreaterThanOrEqual(2);
+  });
+
+  it('each column contains at least one anchor', async () => {
+    await HomePage.open();
+    const footer = await $('footer');
+    const groups = await footer.$$('nav, ul, div:has(a)');
+    const sample = groups.slice(0, 5);
+    for (const g of sample) {
+      const links = await g.$$('a');
+      expect(links.length).toBeGreaterThanOrEqual(1);
+    }
+  });
+
+  it('first column links are visible', async () => {
+    await HomePage.open();
+    const first = await $('footer a');
+    await expect(first).toBeExisting();
+    await expect(first).toBeDisplayed();
+  });
+});
+
+
