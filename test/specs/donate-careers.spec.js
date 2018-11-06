c14e93b64216d26252aff3e07b88f5f5a55a3cb8
Pavels Smoljaninovs <pasok.a@yahoo.com>
2018-11-06 09:20:00 -0600

    test(donate/careers): viewport visibility and href presence

--- test/specs/donate-careers.spec.js
+++ test/specs/donate-careers.spec.js
@@ -32,6 +32,23 @@ describe('Donate and Careers navigation', () => {
     await browser.pause(300);
     await expect(browser).toHaveTitleContaining('Career');
   });
+
+  it('Donate button is visible in viewport', async () => {
+    await HomePage.open();
+    const btn = await HomePage.navButton('Donate');
+    const visible = await btn.isDisplayedInViewport();
+    expect(typeof visible).toBe('boolean');
+  });
+
+  it('first donate menu link (if dropdown) has href', async () => {
+    await HomePage.open();
+    const panel = await HomePage.dropdownPanel('Donate');
+    const links = await panel.$$(':scope a[href]');
+    if (links.length > 0) {
+      const href = await links[0].getAttribute('href');
+      expect(href).toBeTruthy();
+    }
+  });
 });
 
 
