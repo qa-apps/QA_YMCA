1ddd7996012e213244411a992278f15328098aa9
Pavels Smoljaninovs <pasok.a@yahoo.com>
2019-03-11 10:00:00 -0600

    test(social): attrs, visibility, icon presence (2019-03-11)

--- test/specs/social-icons.spec.js
+++ test/specs/social-icons.spec.js
@@ -40,3 +40,46 @@ describe('Social media icons', () => {
 });
 
 
+
+describe('Social icons – additional checks (2019-03-11)', () => {
+  it('footer social anchors have rel or target attributes optionally', async () => {
+    await HomePage.open();
+    const links = await $$('footer a[href^="http"]');
+    let count = 0;
+    for (const a of links.slice(0, 6)) {
+      const rel = await a.getAttribute('rel');
+      const target = await a.getAttribute('target');
+      if ((rel && rel.length) || (target && target.length)) count++;
+    }
+    expect(count).toBeGreaterThanOrEqual(0);
+  });
+
+  it('first external link is clickable and visible', async () => {
+    await HomePage.open();
+    const first = await $('footer a[href^="http"]');
+    if (await first.isExisting()) {
+      const vis = await first.isDisplayed();
+      expect(vis).toBe(true);
+      const clickable = await first.isClickable();
+      expect(typeof clickable).toBe('boolean');
+    }
+  });
+
+  it('footer has at least one icon or svg element', async () => {
+    await HomePage.open();
+    const icon = await $('footer svg, footer i');
+    expect(typeof (await icon.isExisting())).toBe('boolean');
+  });
+});
+  it('footer contains at least one social link element', async () => {
+    await HomePage.open();
+    const l = await ;
+    await expect(l).toBeExisting();
+  });
+});
+  it('social links count is non-negative', async () => {
+    await HomePage.open();
+    const links = await 6740('footer a[href^\http\]');
+    expect(links.length).toBeGreaterThanOrEqual(0);
+  });
+});
