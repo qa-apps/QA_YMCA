6e4e3c068dab1ef89c8e947cca54d927a7e9e146
Pavels Smoljaninovs <pasok.a@yahoo.com>
2019-03-14 09:35:00 -0600

    test(header-search): interactive input, focusability, visibility after submit (2019-03-14)

--- test/specs/header-search.spec.js
+++ test/specs/header-search.spec.js
@@ -40,3 +40,51 @@ describe('Header search behavior', () => {
 });
 
 
+
+describe('Header search – resilience (2019-03-14)', () => {
+  it('search control exists and is interactive when present', async () => {
+    await HomePage.open();
+    const input = await $('header input[type="search"], header input[placeholder*="Search" i]');
+    if (await input.isExisting()) {
+      await input.setValue('YMCA');
+      await expect(input).toHaveValueContaining('Y');
+    }
+  });
+
+  it('submit button or icon is focusable', async () => {
+    await HomePage.open();
+    const btn = await $('header button[type="submit"], header button[aria-label*="search" i]');
+    if (await btn.isExisting()) {
+      await btn.focus();
+      const active = await browser.getActiveElement();
+      const tag = await active.getTagName();
+      expect(tag.length).toBeGreaterThan(0);
+    }
+  });
+
+  it('header still visible after search attempt', async () => {
+    await HomePage.open();
+    const btn = await $('header button[type="submit"], header button[aria-label*="search" i]');
+    if (await btn.isExisting()) {
+      await btn.click();
+      await browser.pause(200);
+    }
+    const header = await $('header');
+    await expect(header).toBeExisting();
+  });
+});
+  it('header contains at least one anchor element', async () => {
+    await HomePage.open();
+    const a = await ;
+    await expect(a).toBeExisting();
+  });
+});
+  it('search button visibility in viewport when available', async () => {
+    await HomePage.open();
+    const btn = await ;
+    if (await btn.isExisting()) {
+      const vis = await btn.isDisplayedInViewport();
+      expect(typeof vis).toBe('boolean');
+    }
+  });
+});
