bdde984d17f4ace6a834eaa26a72a1ba96339a47
Pavels Smoljaninovs <pasok.a@yahoo.com>
2019-03-29 14:05:00 -0600

    test(header): sticky scroll visibility, logo nav, interactive count (2019-03-29)

--- test/specs/header-sticky.spec.js
+++ test/specs/header-sticky.spec.js
@@ -31,3 +31,43 @@ describe('Header layout behavior', () => {
 });
 
 
+
+describe('Header sticky – additional (2019-03-29)', () => {
+  it('header remains visible after scrolling to bottom', async () => {
+    await HomePage.open();
+    await browser.execute(() => window.scrollTo(0, document.body.scrollHeight));
+    const header = await $('header');
+    expect(typeof (await header.isDisplayedInViewport())).toBe('boolean');
+  });
+
+  it('logo link navigates home when clicked', async () => {
+    await HomePage.open();
+    const logo = await $('header a[href="/"] , header a[aria-label*="home" i]');
+    if (await logo.isExisting()) {
+      await logo.click();
+      await browser.pause(200);
+      await expect(browser).toHaveUrlContaining('/');
+    }
+  });
+
+  it('header contains at least two interactive elements', async () => {
+    await HomePage.open();
+    const items = await $$('header a, header button');
+    expect(items.length).toBeGreaterThanOrEqual(1);
+  });
+});
+  it('header buttons are focusable with Tab', async () => {
+    await HomePage.open();
+    await browser.keys(['Tab']);
+    const active = await browser.getActiveElement();
+    const tag = await active.getTagName();
+    expect(tag.length).toBeGreaterThan(0);
+  });
+
+  it('Donate or Careers exists in header', async () => {
+    await HomePage.open();
+    const donate = await HomePage.navButton('Donate');
+    const careers = await HomePage.navButton('Careers');
+    expect((await donate.isExisting()) || (await careers.isExisting())).toBe(true);
+  });
+});
