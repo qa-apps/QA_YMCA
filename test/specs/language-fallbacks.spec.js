4eb0c026f553c7397d89dc9f10c447bc683ae96d
Pavels Smoljaninovs <pasok.a@yahoo.com>
2019-04-04 09:40:00 -0600

    test(i18n): plausible html lang, button/anchor names (2019-04-04)

--- test/specs/language-fallbacks.spec.js
+++ test/specs/language-fallbacks.spec.js
@@ -35,3 +35,41 @@ describe('Language and text fallbacks', () => {
 });
 
 
+
+describe('Language/Accessibility – extended (2019-04-04)', () => {
+  it('html lang attribute exists and is plausible (2–5 chars)', async () => {
+    await HomePage.open();
+    const lang = (await $('html').getAttribute('lang')) || '';
+    expect(lang.length >= 2 && lang.length <= 5).toBe(true);
+  });
+
+  it('buttons provide either text or aria-label', async () => {
+    await HomePage.open();
+    const buttons = await $$('button');
+    let ok = 0;
+    for (const b of buttons.slice(0, 10)) {
+      const name = await b.getAttribute('aria-label');
+      const text = (await b.getText()).trim();
+      if ((name && name.trim()) || text) ok++;
+    }
+    expect(ok).toBeGreaterThanOrEqual(1);
+  });
+
+  it('links contain discernible text for first few anchors', async () => {
+    await HomePage.open();
+    const anchors = await $$('a[href]');
+    let readable = 0;
+    for (const a of anchors.slice(0, 8)) {
+      const t = (await a.getText()).trim();
+      const aria = await a.getAttribute('aria-label');
+      if (t || aria) readable++;
+    }
+    expect(readable).toBeGreaterThanOrEqual(Math.min(2, anchors.length));
+  });
+});
+  it('aria-label present on at least one interactive control', async () => {
+    await HomePage.open();
+    const el = await ;
+    expect(typeof (await el.isExisting())).toBe('boolean');
+  });
+});
