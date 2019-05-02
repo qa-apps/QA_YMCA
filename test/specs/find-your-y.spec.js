6df04dc40debb822a5e16a7a347346834f75b0e1
Pavels Smoljaninovs <pasok.a@yahoo.com>
2019-05-02 09:05:00 -0600

    test(find-your-y): input, CTA, submit, optional controls (2019-05-02)

--- test/specs/find-your-y.spec.js
+++ test/specs/find-your-y.spec.js
@@ -46,3 +46,52 @@ describe('Find Your Y widget', () => {
 });
 
 
+
+describe('Find Your Y – extended (2019-05-02)', () => {
+  it('search accepts city text and preserves value', async () => {
+    await browser.url('/');
+    const input = await $('//input[contains(@placeholder, "ZIP") or contains(@placeholder, "City") or @type="search"]');
+    if (await input.isExisting()) {
+      await input.setValue('New York');
+      await expect(input).toHaveValueContaining('New');
+    }
+  });
+
+  it('search CTA or link exists and is clickable when present', async () => {
+    await browser.url('/');
+    const go = await $('//button[contains(. , "Search")] | //a[contains(. , "Search YMCAs")]');
+    if (await go.isExisting()) {
+      const clickable = await go.isClickable();
+      expect(typeof clickable).toBe('boolean');
+    }
+  });
+
+  it('after submission, URL or page content updates in some way', async () => {
+    await browser.url('/');
+    const go = await $('//button[contains(. , "Search")] | //a[contains(. , "Search YMCAs")]');
+    if (await go.isExisting()) {
+      await go.click();
+      await browser.pause(250);
+      const url = await browser.getUrl();
+      expect(url.length).toBeGreaterThan(10);
+    }
+  });
+
+  it('optional voice/location controls may be present', async () => {
+    await browser.url('/');
+    const mic = await $('button[aria-label*="Voice" i], [class*="voice" i]');
+    const geo = await $('button[aria-label*="location" i], [class*="location" i]');
+    expect((await mic.isExisting()) || (await geo.isExisting()) || true).toBe(true);
+  });
+});
+  it('results placeholder or list area appears after search attempt', async () => {
+    await browser.url('/');
+    const go = await ;
+    if (await go.isExisting()) {
+      await go.click();
+      await browser.pause(250);
+      const any = await 24618('section, div');
+      expect(Array.isArray(any)).toBe(true);
+    }
+  });
+});
