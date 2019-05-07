df00a226d23facb50e2176c9837bc103027d0dc0
Pavels Smoljaninovs <pasok.a@yahoo.com>
2019-05-07 11:10:00 -0600

    test(find-nearby): ZIP entry, focusable CTA, submit behavior (2019-05-07)

--- test/specs/find-nearby-widget.spec.js
+++ test/specs/find-nearby-widget.spec.js
@@ -39,3 +39,46 @@ describe('Find nearby widget behavior', () => {
 });
 
 
+
+describe('Find nearby widget – extended (2019-05-07)', () => {
+  it('ZIP input accepts numeric string and retains it', async () => {
+    await HomePage.open();
+    const input = await $('//input[contains(@placeholder, "ZIP") or @type="search"]');
+    if (await input.isExisting()) {
+      await input.setValue('60616');
+      await expect(input).toHaveValueContaining('606');
+    }
+  });
+
+  it('search button exists and is focusable when present', async () => {
+    await HomePage.open();
+    const go = await $('//button[contains(. , "Search")] | //a[contains(. , "Search YMCAs")]');
+    if (await go.isExisting()) {
+      await go.focus();
+      const active = await browser.getActiveElement();
+      const tag = await active.getTagName();
+      expect(tag.length).toBeGreaterThan(0);
+    }
+  });
+
+  it('submits and evaluates page title/URL minimal change', async () => {
+    await HomePage.open();
+    const go = await $('//button[contains(. , "Search")] | //a[contains(. , "Search YMCAs")]');
+    if (await go.isExisting()) {
+      await go.click();
+      await browser.pause(250);
+      const title = await browser.getTitle();
+      expect(title.length >= 0).toBe(true);
+    }
+  });
+});
+  it('input keeps value after blur', async () => {
+    await HomePage.open();
+    const input = await ;
+    if (await input.isExisting()) {
+      await input.setValue('10001');
+      await .click();
+      await expect(input).toHaveValueContaining('10001');
+    }
+  });
+});
